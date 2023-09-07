f3172dc5e5d12f03877c9fa3c48a7d70ec6a2b0a
Gabriela Leichtweis <gabrielaleichtweis@gmail.com>
2023-09-07 11:08:49 -0300

    test: testa list-notes.usecase

---
+++ tests/app/features/notes/usecases/list-notes.usecase.spec.ts
@@ -0,0 +1,63 @@
+import { ListNotesUsecase } from '../../../../../src/app/features/note/usecases/list-notes.usecase';
+import { NotesRepository } from '../../../../../src/app/features/note/repositories/note.respository';
+import { User } from '../../../../../src/app/models/user.model';
+import { CacheRepository } from '../../../../../src/app/shared/database/repositories/cache.repository';
+import { Database } from '../../../../../src/main/database/database.connection';
+import { CacheDatabase } from '../../../../../src/main/database/redis.connection';
+import { Note } from '../../../../../src/app/models/note.model';
+
+describe('Testes para o list-users usecase', () => {
+  beforeAll(async () => {
+    await Database.connect();
+    await CacheDatabase.connect();
+
+    jest.setTimeout(30000);
+  });
+
+  afterAll(async () => {
+    await Database.connection.destroy();
+    await CacheDatabase.connection.quit();
+  });
+
+  const createSut = () => {
+    const cacheRepository = new CacheRepository();
+    const notesRepository = new NotesRepository();
+
+    const sut = new ListNotesUsecase(cacheRepository, notesRepository);
+
+    return sut;
+  };
+
+  test('Deveria retornar sucesso se houverem recados em cache para listar', async () => {
+    const sut = createSut();
+
+    jest.spyOn(CacheRepository.prototype, 'get').mockResolvedValue(true);
+
+    const result = await sut.execute({
+      email: 'any_email',
+    });
+
+    expect(result).toBeDefined();
+    expect(result.ok).toBe(true);
+    expect(result.code).toEqual(200);
+    expect(result).toHaveProperty('message', 'Recados listados com sucesso');
+    expect(result).toHaveProperty('data');
+  });
+
+  test('Deveria retornar sucesso se houverem recados sem cache para listar', async () => {
+    const sut = createSut();
+
+    jest.spyOn(CacheRepository.prototype, 'get').mockResolvedValue(undefined);
+    jest.spyOn(CacheRepository.prototype, 'setEx').mockResolvedValue();
+
+    const result = await sut.execute({
+      email: 'any_email',
+    });
+
+    expect(result).toBeDefined();
+    expect(result.ok).toBe(true);
+    expect(result.code).toEqual(200);
+    expect(result).toHaveProperty('message', 'Recados listados com sucesso');
+    expect(result).toHaveProperty('data');
+  });
+});
